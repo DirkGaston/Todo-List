aba295e32d260e9296f9ca012c6bd1a4924e44c9
DirkGaston <dirkgaston87@gmail.com>
2022-07-26 08:59:15 -0400

    función check y contador de tareas realizadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,61 @@
 const tasks = [
-  { id: 0, description: "Ir al gym", done: false },
-  { id: 1, description: "Leer", done: false },
-  { id: 2, description: "Programar", done: false },
+  { id: 0, description: "Ir al gym", done: false, isChecked: false },
+  { id: 1, description: "Leer", done: false, isChecked: false },
+  { id: 2, description: "Programar", done: false, isChecked: false },
 ];
 const taskList = document.querySelector(".tasks");
 const taskInput = document.querySelector("#taskDesc");
 const addTask = document.querySelector("#addTask");
 const taskHeader = "<tr><th>ID</th><th>Tarea</th></tr>";
 const taskCounter = document.querySelector("#taskCounter");
+const doneCounter = document.querySelector("#doneCounter");
 let id = tasks.length;
+let htmlCode = "";
+
+const template = (task) => {
+  return `<tr class="taskRow">
+  <td>${task.id}</td>
+  <td>${task.description}</td>
+  <td><input type="checkbox" id="cbox${task.id}" onclick="checkTask(${task.id})" checked="${task.isChecked}" /></td>
+  <td class="deleteTaskIcon">
+  <i onclick="deleteTask(${task.id})" class="fa-solid fa-x deleteTaskIcon"></i>
+  </td>
+</tr>`;
+};
+
+const pageLoad = () => {
+  for (const task of tasks) {
+    htmlCode += template(task);
+  }
+  taskList.innerHTML = taskHeader + htmlCode;
+  taskCounter.innerHTML = tasks.length;
+  filterDone();
+};
 
 function renderTasks() {
   let htmlCode = "";
   for (let task of tasks) {
-    htmlCode += ` <tr>
-    <td>${task.id}</td>
-    <td>${task.description}</td>
-    <td><input type="checkbox" id="cbox" /></td>
-    <td class="deleteTaskIcon">
-    <i onclick="deleteTask(${task.id})" class="fa-solid fa-x deleteTaskIcon"></i>
-    </td>
-  </tr>`;
+    htmlCode += template(task);
     id = tasks.length;
   }
   taskList.innerHTML = taskHeader + htmlCode;
   taskCounter.innerHTML = tasks.length;
+  filterDone();
 }
 
 addTask.addEventListener("click", () => {
   newTask = { id: id, description: taskInput.value, done: false };
-  console.log(newTask);
   tasks.push(newTask);
   taskInput.value = "";
 
   let htmlCode = "";
   for (let task of tasks) {
-    htmlCode += ` <tr>
-    <td>${task.id}</td>
-    <td>${task.description}</td>
-    <td><input type="checkbox" id="cbox" /></td>
-    <td class="deleteTaskIcon">
-    <i onclick="deleteTask(${task.id})" class="fa-solid fa-x deleteTaskIcon"></i>
-    </td>
-  </tr>`;
+    htmlCode += template(task);
     id = tasks.length;
   }
   taskList.innerHTML = taskHeader + htmlCode;
   taskCounter.innerHTML = tasks.length;
+  filterDone();
 });
 
 function deleteTask(id) {
@@ -55,62 +64,27 @@ function deleteTask(id) {
   renderTasks();
 }
 
-// // Crear un botón de eliminación y adjuntarlo a cada item de la lista
-// const myNodelist = document.getElementsByTagName("li");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   let span = document.createElement("SPAN");
-//   let txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
+function checkTask(id) {
+  const index = tasks.findIndex((ele) => ele.id == id);
+  let checkbox = document.querySelector("#cbox" + id);
+  if (checkbox.checked == true) {
+    tasks[index].done = true;
+  } else {
+    tasks[index].done = false;
+  }
+  let taskToStrikeThrough = document.getElementById(id);
+  taskToStrikeThrough.innerHTML =
+    "<strike>" + taskToStrikeThrough.innerHTML + "</strike>";
 
-// // Hacer click sobre el botón de eliminación para eliminar la tarea seleccionada
-// const close = document.getElementsByClassName("close");
-// let i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function () {
-//     let div = this.parentElement;
-//     div.style.display = "none";
-//   };
-// }
+  filterDone();
+}
 
-// // Añadir un símbolo CHECK al marcar una tarea como hecha
-// const list = document.querySelector("ul");
-// list.addEventListener(
-//   "click",
-//   function (ev) {
-//     if (ev.target.tagName === "li") {
-//       ev.target.classList.toggle("checked");
-//     }
-//   },
-//   false
-// );
+function filterDone() {
+  let doneTasks = tasks.filter((task) => task.done == true);
+  let totalDoneTasks = doneTasks.length;
+  doneCounter.innerHTML = totalDoneTasks;
+}
 
-// // Crear nuevo item en lista al hacer click sobre el botón AÑADIR
-// function newElement() {
-//   let li = document.createElement("li");
-//   let inputValue = document.getElementById("myInput").value;
-//   let t = document.createTextNode(inputValue);
-//   li.appendChild(t);
-//   if (inputValue === "") {
-//     alert("¡Debes escribir algo!");
-//   } else {
-//     document.getElementById("myUL").appendChild(li);
-//   }
-//   document.getElementById("myInput").value = "";
-
-//   let span = document.createElement("SPAN");
-//   let txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   li.appendChild(span);
-
-//   for (i = 0; i < close.length; i++) {
-//     close[i].onclick = function () {
-//       let div = this.parentElement;
-//       div.style.display = "none";
-//     };
-//   }
-// }
+window.onload = function () {
+  pageLoad();
+};
